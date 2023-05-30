d65aff4f6097df007af92c84eecc88cd53b7a0fa
Hyung Kyu <onlymuk@naver.com>
2023-05-30 15:12:02 +0900

    fifth commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,47 @@
 import React, { useState } from "react";
 //Hook
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
+  //   const [show, setShow] = useState(true);
+  //   const [operator, setOperator] = useState(operators[0]);
+
+  const operators = ["+", "-", "*"];
+
+  const [info, setInfo] = useState({
+    count: 0,
+    show: true,
+    operator: operators[0],
+  });
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button
+        onClick={() => {
+          let result;
+          if (info.operator === "+") result = info.count + 1;
+          if (info.operator === "-") result = info.count - 1;
+          if (info.operator === "*") result = info.count * 1;
+          setInfo({ info, count: result });
+        }}
+      >
+        {info.operator}1
+      </button>
+      <button
+        onClick={() => {
+          setInfo({ ...info, show: !info.show });
+        }}
+      >
+        Show and Hide
+      </button>
+      <button
+        onClick={() => {
+          const idx = Math.floor(Math.random() * operators.length);
+          setInfo({ ...info, operator: operators[idx] });
+        }}
+      >
+        Change Operator
+      </button>
       <br />
-      Counter: {count}
+      {info.show && `Counter: ${info.count}`}
     </div>
   );
 }
